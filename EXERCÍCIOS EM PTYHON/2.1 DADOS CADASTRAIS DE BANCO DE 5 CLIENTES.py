47bc2ae49954931df684a140df2b7c6ed65497d0
Samuel Alves <alvess7221@gmail.com>
2023-11-17 11:08:28 -0300

    commit 3 - 17/11/2023

--- EXERCÍCIOS EM PTYHON/2.1 DADOS CADASTRAIS DE BANCO DE 5 CLIENTES.py
+++ EXERCÍCIOS EM PTYHON/2.1 DADOS CADASTRAIS DE BANCO DE 5 CLIENTES.py
@@ -11,4 +11,17 @@ for _ in range(5):
     op = input("Digite a operação (C para crédito e D para débito): ")
     clientes.append([num_conta, nome, saldo, op])
     print("-" * 60)
-print(clientes)
+
+#Adicionando os dados na matriz 5x5
+for i in range(5):
+    for j in range(5):
+        if i == j:
+            matriz[i][j] = clientes[i][2]
+        elif clientes[i][3] == 'C':
+            matriz[i][j] = clientes[i][2]
+        elif clientes[i][3] == 'D':
+            matriz[i][j] = -clientes[i][2]
+        
+print("\n Martriz de movimento bancário: ")
+for linha in (matriz):
+    print(linha)
