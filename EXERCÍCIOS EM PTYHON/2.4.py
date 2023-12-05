50353885fbe76e7aaa7a380dbff855d9143223a2
Samuel Alves <alvess7221@gmail.com>
2023-12-05 10:56:46 -0300

    commit 5.2 - 05/12/2023

--- EXERCÍCIOS EM PTYHON/2.4.py
+++ EXERCÍCIOS EM PTYHON/2.4.py
@@ -1,18 +1,42 @@
 from random import randint
-matriz = [[0]*7 for _ in range(12)]
 
-cadastros = []
 
-for _ in range(2):
-    posição = int(input("Posição: "))
-    nome = input("Nome: ")
-    pontos = int(input("Pontos: "))
-    jogos = int(input("Q° jogos: "))
-    vitorias = int(input("Vitórias: "))
-    empates = int(input("Empates: "))
-    derrotas = int(input("Derrotas: "))
-    cadastros.append([posição, nome, pontos, jogos, vitorias, empates, derrotas])
-    print("-="*30)
+def tabela():
+        
+    tabela = []
 
-for linha in (cadastros):
-    print(linha)
+    tabela.append(["L", "Time       ", "P", "V", "E", "D"])
+
+    for _ in range(2, 4):
+        posição = int(input("Posição: "))
+        nome = input("Nome: ")
+        pontos = int(input("Pontos: "))
+        jogos = int(input("Q° jogos: "))
+        vitorias = int(input("Vitórias: "))
+        empates = int(input("Empates: "))
+        derrotas = int(input("Derrotas: "))
+
+        tabela.append([posição, nome, pontos, jogos, vitorias, empates, derrotas])
+        print("-="*30)
+
+    tabela.sort(key=lambda x: x[2], reverse=True)
+
+    l = len(tabela)
+    c = len(tabela[0])
+
+    for i in range(l):
+        for j in range(c):
+            print(tabela[i][j], end="\t")
+        print()
+
+        
+    return tabela
+
+def ordenar_resultados(tabela):
+    #Irá organizar a tabela por meio dos pontos!
+    tabela.sort(key=lambda x: x[2], reverse=True)
+    return tabela
+
+
+
+tabela()
--- EXERCÍCIOS EM PTYHON/teste1.py
+++ EXERCÍCIOS EM PTYHON/teste1.py
@@ -1,53 +1,58 @@
-A = [[2, 7, 6], [9, 5, 1], [4, 3, 18]]
+def criar_tabela():
+    # Cria uma matriz vazia (12 x 7)
+    tabela = [[0] * 7 for _ in range(12)]
 
-for linha in (A):
-    print(linha)
+    # Preenche a matriz com as informações fornecidas pelo usuário
+    for i in range(12):
+        print(f"Informe os dados para o time {i + 1}:")
+        tabela[i][0] = int(input("Posição: "))
+        tabela[i][1] = input("Time: ")
+        tabela[i][2] = int(input("Pontos: "))
+        tabela[i][3] = int(input("Jogos: "))
+        tabela[i][4] = int(input("Vitórias: "))
+        tabela[i][5] = int(input("Empates: "))
+        tabela[i][6] = int(input("Derrotas: "))
+        print()
 
-def QuadradoMagico(A):
-    soma_linha = []
-    for i in range(3):
-        soma = 0
-        for j in range(3):
-            soma += A[i][j]
-        soma_linha.append(soma)
+    return tabela
 
-    soma_colunas = []
-    for j in range(3):
-        soma = 0
-        for i in range(3):
-            soma += A[i][j]
-        soma_colunas.append(soma)
-
-    soma_diagonal1 = 0
-    for i in range(3):
-        soma_diagonal1 += A[i][i]
-
-    soma_diagonal2 = 0
-    for i in range(3):
-        soma_diagonal2 += A[i][3 - 1 - i]
-
-
-    #Equiparar os valores
-
-    for elemento in (soma_linha):
-        if elemento != soma_diagonal2:
-            return False
-
-    for elemento in (soma_colunas):
-        if elemento != soma_diagonal2:
-            return False
+def imprimir_tabela(tabela):
+    # Imprime a tabela formatada
+    print("{:<5} {:<20} {:<10} {:<8} {:<8} {:<8} {:<8}".format(
+        "Pos", "Time", "Pontos", "Jogos", "Vitórias", "Empates", "Derrotas"))
     
-    if soma_diagonal1 != soma_diagonal2:
-        return False
-    
-    return True
+    for linha in tabela:
+        print("{:<5} {:<20} {:<10} {:<8} {:<8} {:<8} {:<8}".format(*linha))
 
+def calcular_resultados(tabela):
+    # Ordena a tabela pela pontuação em ordem decrescente
+    tabela.sort(key=lambda x: x[2], reverse=True)
 
+    # a) Campeão brasileiro
+    campeao = tabela[0][1]
 
-if (QuadradoMagico(A)):
-    print("É um QUADRADO MÁGICO")
-else:
-    print("NÃO é um QUADRADO MÁGICO")
-    
+    # b) 5 primeiros colocados para a Libertadores
+    libertadores = [tabela[i][1] for i in range(5)]
 
-    
+    # c) 5 seguintes para a Copa Sul-Americana
+    sul_americana = [tabela[i][1] for i in range(5, 10)]
+
+    # d) 2 últimos rebaixados para a Série B
+    rebaixados = [tabela[i][1] for i in range(10, 12)]
+
+    return campeao, libertadores, sul_americana, rebaixados
+
+# Criar a tabela
+tabela = criar_tabela()
+
+# Imprimir a tabela
+imprimir_tabela(tabela)
+
+# Calcular e imprimir os resultados
+campeao, libertadores, sul_americana, rebaixados = calcular_resultados(tabela)
+
+print("\nResultados:")
+print(f"a) Campeão brasileiro: {campeao}")
+print(f"b) 5 primeiros colocados (Libertadores): {', '.join(libertadores)}")
+print(f"c) 5 seguintes (Copa Sul-Americana): {', '.join(sul_americana)}")
+print(f"d) 2 últimos rebaixados para a Série B: {', '.join(rebaixados)}")
