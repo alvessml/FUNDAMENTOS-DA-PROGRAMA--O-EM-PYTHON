0464ce544e92321088f03ff0b3a5ab0a4b9a075e
Samuel Alves <alvess7221@gmail.com>
2023-12-06 10:09:16 -0300

    commit 16 - 06/12/2023

--- EXERCÍCIOS EM PTYHON/2.4.py
+++ EXERCÍCIOS EM PTYHON/2.4.py
@@ -1,13 +1,13 @@
 from random import randint
 
 
-def tabela():
-        
+def tabela_classicação():
+
     tabela = []
 
-    tabela.append(["L", "Time       ", "P", "V", "E", "D"])
+    #tabela.append(["L", "Time       ", "P", "V", "E", "D"])
 
-    for _ in range(2, 4):
+    for _ in range(12):
         posição = int(input("Posição: "))
         nome = input("Nome: ")
         pontos = int(input("Pontos: "))
@@ -28,15 +28,37 @@ def tabela():
         for j in range(c):
             print(tabela[i][j], end="\t")
         print()
-
         
-    return tabela
+    return tabela_classicação
 
 def ordenar_resultados(tabela):
     #Irá organizar a tabela por meio dos pontos!
     tabela.sort(key=lambda x: x[2], reverse=True)
-    return tabela
+
+    # a) campeão do brasileirão 
+    campeao = tabela[0][0]
+
+    # b) 5 primeiros colocados para a libertadores
+    libertadores = [tabela[i][0] for i in range(5)]
+
+    # c) 5 seguintes para a copa sul-americano
+    sul_americana = [tabela[i][0] for i in range(5, 10)]
+
+    # d) 2 últimos que serão rebaixados
+    rebaixados = [tabela[i][0] for i in range(10, 12)]
+
+    return campeao, libertadores, sul_americana, rebaixados
+
+def barra(tabela):
+     tabela.append(["L", "Time       ", "P", "V", "E", "D"])
 
 
+tabela_classicação()
 
-tabela()
+campeao, libertadores, sul_americana, rebaixados = ordenar_resultados
+
+print("\n Resultados do brasileirão: ")
+print(f"a) Campeão do brasileiro: {campeao}")
+print(f"b) 5 primeiros que irão para a libertadores: {", ".join(libertadores)}")
+print(f"5 seguintes que irão para a sul-americana: {", ".join(sul_americana)}")
+print(f"Os dois últimos que serão rebaixados: {", ".join(rebaixados)}")
