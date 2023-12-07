9dc61cfb8a5d7fc78f4ecd9eaf3ef25f28a918ab
Samuel Alves <alvess7221@gmail.com>
2023-12-07 10:44:23 -0300

    commit 17 - 07/12/2023

--- EXERCÍCIOS EM PTYHON/2.4.py
+++ EXERCÍCIOS EM PTYHON/2.4.py
@@ -1,66 +1,39 @@
-from random import randint
+tabela = []
 
+for x in range(6):
+    posição = []
+    nome = input("Nome: ")
+    pontos = int(input("Pontos: "))
+    jogos = int(input("Q° jogos: "))
+    vitorias = int(input("Vitórias: "))
+    empates = int(input("Empates: "))
+    derrotas = int(input("Derrotas: "))
 
-def tabela_classicação():
+    tabela.append([posição, nome, pontos, jogos, vitorias, empates, derrotas])
 
-    tabela = []
+    #maior = pontos
 
-    #tabela.insert(0, ["L", "Time       ", "P", "V", "E", "D"])
+    #if pontos > maior
+    print("-="*30)
 
-    for _ in range(3):
-        posição = int(input("Posição: "))
-        nome = input("Nome: ")
-        pontos = int(input("Pontos: "))
-        jogos = int(input("Q° jogos: "))
-        vitorias = int(input("Vitórias: "))
-        empates = int(input("Empates: "))
-        derrotas = int(input("Derrotas: "))
+#-----------------------------------------------------------#
+tabela.sort(key=lambda x: x[2], reverse=True)
 
-        tabela.append([posição, nome, pontos, jogos, vitorias, empates, derrotas])
-        print("-="*30)
+tabela_topo = ["L", "Time       ", "P", "V", "E", "D"]
 
-    tabela.sort(key=lambda x: x[2], reverse=True)
+tabela.insert(0, tabela_topo)
 
-    l = len(tabela)
-    c = len(tabela[0])
+l = len(tabela)
+c = len(tabela[0])
 
-    for i in range(l):
-        for j in range(c):
-            print(tabela[i][j], end="\t")
-        print()
-        
-    return tabela_classicação, tabela_topo
+for i in range(l):
+    for j in range(c):
+        print(tabela[i][j], end="\t")
+    print()
 
-def ordenar_resultados(tabela):
-
-    tabela_topo = ["L", "Time       ", "P", "V", "E", "D"]
-
-    tabela.insert(0, tabela_topo)
-
-    #Irá organizar a tabela por meio dos pontos!
-
-    # a) campeão do brasileirão 
-    campeao = tabela[1][1]
-
-    # b) 5 primeiros colocados para a libertadores
-    libertadores = [tabela[i][0] for i in range(1, 6)]
-
-    # c) 5 seguintes para a copa sul-americano
-    sul_americana = [tabela[i][0] for i in range(6, 11)]
-
-    # d) 2 últimos que serão rebaixados
-    rebaixados = [tabela[i][0] for i in range(11, 13)]
-
-    return campeao, libertadores, sul_americana, rebaixados
-
-
-
-tabela_classicação()
-
-campeao, libertadores, sul_americana, rebaixados = ordenar_resultados
-
-print("\n Resultados do brasileirão: ")
+#-----------------------------------------------------------#
+print(f"\n Resultados do brasileirão: {tabela[1][1]}")
 print(f"a) Campeão do brasileiro: {campeao}")
-print(f"b) 5 primeiros que irão para a libertadores: {", ".join(libertadores)}")
-print(f"5 seguintes que irão para a sul-americana: {", ".join(sul_americana)}")
-print(f"Os dois últimos que serão rebaixados: {", ".join(rebaixados)}")
+print(f"b) 5 primeiros que irão para a libertadores: {", ".join([tabela[i][0] for i in range(1, 2)])}")
+print(f"5 seguintes que irão para a sul-americana: {", ".join([tabela[i][0] for i in range(2, 3)])}")
+print(f"Os dois últimos que serão rebaixados: {", ".join([tabela[i][0] for i in range(3, 6)])}")
