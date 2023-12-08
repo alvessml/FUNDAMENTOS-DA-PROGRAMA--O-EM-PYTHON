1e660fe82cfe2b8b0c4e6d43396b39fd18588537
Samuel Alves <alvess7221@gmail.com>
2023-12-08 18:42:22 -0300

    commit 18.4 - 08/12/2023

--- EXERCÍCIOS EM PTYHON/teste.py
+++ EXERCÍCIOS EM PTYHON/teste.py
@@ -1,44 +1,18 @@
+from datetime import date
+
 def usuario():
-    cadastro = []
+    data_str = input("Digite uma data qualquer ex:(dd/mm/aaaa): ")
 
-    for _ in range(12):
-        nome = input("Time: ")
-        pontos = int(input("Pontos: "))
-        jogos = int(input("Q° jogos: "))
-        vitorias = int(input("Vitórias: "))
-        empates = int(input("Empates: "))
-        derrotas = int(input("Derrotas: "))
+    # método split irá separá a data_str em cada "/", a qual foi o critério.
+    dia, mes, ano = map(int, data_str.split("/"))
+    return dia, mes, ano
 
-        cadastro.append([nome, pontos, jogos, vitorias, empates, derrotas])
+def organização(dia, mes, ano):
+    hoje = date.today()
 
-        print("-=" * 30)
+    
 
-    return cadastro
+    dias_faltando = map() 
 
-def organizacao(cadastro):
-    cadastro.sort(key=lambda x: x[2], reverse=True)
 
-    posicao = [i + 1 for i in range(12)]
-
-    tabela = [["L"] + ["Time", "P", "J", "V", "E", "D"]] + [[posicao[i]] + linha for i, linha in enumerate(cadastro)]
-
-    return tabela
-
-def classificacao(tabela):
-    print(f"\n Resultados do brasileirão: ")
-    print(f"a) Campeão do brasileiro: {tabela[1][1]}")
-    print(f"b) 5 primeiros que irão para a libertadores: {', '.join([tabela[i][1] for i in range(1, 6)])}")
-    print(f"c) 5 seguintes que irão para a sul-americana: {', '.join([tabela[i][1] for i in range(6, 11)])}")
-    print(f"d) Os dois últimos que serão rebaixados: {', '.join([tabela[i][1] for i in range(11, 13)])}")
-
-def main():
-    cadastro = usuario()
-    tabela = organizacao(cadastro)
-    classificacao(tabela)
-
-    # Exibindo a tabela
-    for linha in tabela:
-        print("\t".join(map(str, linha)))
-
-if __name__ == "__main__":
-    main()
+    return dias_faltando
--- EXERCÍCIOS EM PTYHON/teste1.py
+++ EXERCÍCIOS EM PTYHON/teste1.py
@@ -1,47 +1,5 @@
-def usuario():
-    cadastro = []
+from datetime import date
 
-    for _ in range(12):
-        nome = input("Time: ")
-        pontos = int(input("Pontos: "))
-        jogos = int(input("Q° jogos: "))
-        vitorias = int(input("Vitórias: "))
-        empates = int(input("Empates: "))
-        derrotas = int(input("Derrotas: "))
+hoje = date.today()
 
-        cadastro.append([nome, pontos, jogos, vitorias, empates, derrotas])
-        
-        print("-="*30)
-
-    return cadastro
-
-def organização(cadastro):
-    cadastro.sort(key=lambda x: x[1], reverse=True)
-
-    posição = [i + 1 for i in range(12)]
-
-    tabela = [["L", "Time       ", "P", "J", "V", "E", "D"]] + [[posição[i]] + linha for i, linha in enumerate(cadastro)]
-
-    return tabela
-
-def classificação(tabela):
-    print(f"\n Resultados do brasileirão: ")
-    print(f"\na) Campeão do brasileiro: {tabela[1][1]}")
-    print(f"\nb) 5 primeiros que irão para a libertadores: {", ".join([tabela[i][1] for i in range(1, 6)])}")
-    print(f"\nc) 5 seguintes que irão para a sul-americana: {", ".join([tabela[i][1] for i in range(6, 11)])}")
-    print(f"\nd)Os dois últimos que serão rebaixados: {", ".join([tabela[i][1] for i in range(11, 13)])}")
-
-
-# Conduzindo o programa com a sequência abaixo:
-def main():
-    cadastro = usuario()
-    tabela = organização(cadastro)
-
-    #Exibir na tela:
-    for linha in (tabela):
-        print("\t".join(map(str, linha)))
-        # map(str, linha): Converte cada elemento da linha para uma string, # pois join espera strings como argumentos.
-
-    classificação(tabela)
-
-main()
+print(hoje)
