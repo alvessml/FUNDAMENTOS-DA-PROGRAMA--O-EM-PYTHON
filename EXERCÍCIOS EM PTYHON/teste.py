92862dc7a6c94ceccd2e80bc57cbe785140ba079
Samuel Alves <alvess7221@gmail.com>
2023-12-09 17:31:02 -0300

    commit 19.4 - 09/12/2023

--- EXERCÍCIOS EM PTYHON/teste.py
+++ EXERCÍCIOS EM PTYHON/teste.py
@@ -2,35 +2,31 @@ def q_cédulas():
     valor = int(input("Digite o quantos você deseja tira do saldo (acima de R$3,99): R$"))
 
     cédulas = []
-    while valor > 0:
-        if valor >= 200:
-            valor -= 200
-            cédulas.append(200)
-        elif 100 <= valor < 200:
-            valor -= 100
-            cédulas.append(100)
-        elif 50 <= valor < 100:        
-            valor -= 50
-            cédulas.append(50)
-        elif 20 <= valor < 50:       
-            valor -= 20
-            cédulas.append(20)
-        elif 10 <= valor < 20:
-            valor -= 10
-            cédulas.append(10)  
-        elif 2 <= valor < 10:
-            if valor % 2 == 1:
-                valor -= 5
-                cédulas.append(2)
-            else:
-                valor -= 2
-                cédulas.append(2)
+    total = valor
+    céd_disponíveis = 200
+    while True:
+        if total > céd_disponíveis:
+            total -= céd_disponíveis
+            cédulas.append(céd_disponíveis)
         else:
-            print("Não é possível fornecer cédula!")
-            break
+            if céd_disponíveis == 50:
+                céd_disponíveis = 20
+            elif céd_disponíveis == 20:
+                céd_disponíveis = 10
+            elif céd_disponíveis == 10:
+                céd_disponíveis = 5
+            elif céd_disponíveis == 5:
+                céd_disponíveis = 2
+            if total < 4:
+                break
     return cédulas
 def main():
     cédulas = q_cédulas()
-    print(f"Você receberá {} cédulas")
+    cédulas_unicas = set(cédulas)
+    print("Você reberá", end="")
+    for elemento in cédulas_unicas:
+        quantidade = cédulas.count(elemento)
+        print(f", {quantidade} cédulas de {elemento} reais", end="")
+    print(".")
 
 main()
