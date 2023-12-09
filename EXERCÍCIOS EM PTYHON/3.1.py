8e803179096070569bff7929d8f1dc996ec15e77
Samuel Alves <alvess7221@gmail.com>
2023-12-09 13:36:46 -0300

    commit 19.1 - 09/12/2023

---
+++ EXERCÍCIOS EM PTYHON/3.1.py
@@ -0,0 +1,41 @@
+from datetime import date
+
+def usuario():
+    data_str = input("Digite uma data qualquer ex:(dd/mm/aaaa): ")
+
+    # método split irá separá a data_str em cada "/", a qual foi o critério.
+    dia, mes, ano = map(int, data_str.split("/"))
+    return dia, mes, ano
+
+def organização(dia, mes, ano):
+    data_atual = date.today()
+
+    day = data_atual.day
+    month = data_atual.month
+    year = data_atual.year
+    
+    if dia >= day:
+        dia_f = dia - day
+    else:
+        dia_f = day - dia
+    
+    if mes >= month:
+        mes_f = mes - month
+    else:
+        mes_f = month - mes
+
+    if ano >= year:
+        ano_f = ano - year
+    else:
+        ano_f = year - ano
+
+    dia_totais_f = dia_f + (mes_f * 30) + (ano_f * 365)
+
+    return dia_totais_f
+  
+def main():
+    dia, mes, ano = usuario()
+    dia_totais_f = organização(dia, mes, ano)
+    print("Você ")
+
+main()
--- EXERCÍCIOS EM PTYHON/teste.py
+++ EXERCÍCIOS EM PTYHON/teste.py
@@ -13,11 +13,29 @@ def organização(dia, mes, ano):
     day = data_atual.day
     month = data_atual.month
     year = data_atual.year
-
-    dias
-
-def main():
-    usuario()
     
+    if dia >= day:
+        dia_f = dia - day
+    else:
+        dia_f = day - dia
+    
+    if mes >= month:
+        mes_f = mes - month
+    else:
+        mes_f = month - mes
+
+    if ano >= year:
+        ano_f = ano - year
+    else:
+        ano_f = year - ano
+
+    dia_totais_f = dia_f + (mes_f * 30) + (ano_f * 365)
+
+    return dia_totais_f
+  
+def main():
+    dia, mes, ano = usuario()
+    dia_totais_f = organização(dia, mes, ano)
+    print(dia_totais_f)
 
 main()
