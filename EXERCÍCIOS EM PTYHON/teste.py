5d92b6caa38e10157fe1a6db4ecf0121b08f07c2
Samuel Alves <alvess7221@gmail.com>
2023-12-10 12:45:48 -0300

    commit 20.1 - 10/12/2023

--- EXERCÍCIOS EM PTYHON/teste.py
+++ EXERCÍCIOS EM PTYHON/teste.py
@@ -1,39 +1,26 @@
-def q_cédulas():
-    valor = int(input("Digite o quantos você deseja tira do saldo (acima de R$3,99): R$"))
+def caixa_eletronico():
+    valor_saque = int(input("Digite o quantos você deseja tira do saldo (acima de R$3,99): R$"))
 
-    cédulas = []
-    total = valor
-    céd_disponíveis = 200
-    tot_ced = 0   
-    while True:
-        if total > céd_disponíveis:
-            total -= céd_disponíveis
-            cédulas.append(céd_disponíveis)
-            tot_ced += 1    
-        else:
-            if tot_ced > 0:
-                print(f"Total de {tot_ced} cédulas de R{céd_disponíveis}")
-            if céd_disponíveis == 100:
-                céd_disponíveis = 50
-            if céd_disponíveis == 50:
-                céd_disponíveis = 20
-            elif céd_disponíveis == 20:
-                céd_disponíveis = 10
-            elif céd_disponíveis == 10:
-                céd_disponíveis = 5
-            elif céd_disponíveis == 5:
-                céd_disponíveis = 2
-            tot_ced = 0
-            if total == 0:
-                break
-    return cédulas
+    cédulas = [200, 100, 50, 20, 10, 5, 2]
+    notas = []
+    for nota in cédulas:
+        while True:
+                if valor_saque >= nota:
+                    if valor_saque % 2 == 0: 
+                        valor_saque -= nota
+                        notas.append(nota)
+                        break
+                elif cédulas[nota + 1] <= valor_saque < cédulas[nota - 1]: 
+                    valor_saque -= nota
+                    notas.append(nota)
+                    break
+                else:
+                    break
+                
+    return notas
+
 def main():
-    cédulas = q_cédulas()
-    cédulas_unicas = set(cédulas)
-    print("Você reberá", end="")
-    for elemento in cédulas_unicas:
-        quantidade = cédulas.count(elemento)
-        print(f", {quantidade} cédulas de {elemento} reais", end="")
-    print(".")
+    notas = caixa_eletronico()
+    print(notas)
 
 main()
--- EXERCÍCIOS EM PTYHON/teste1.py
+++ EXERCÍCIOS EM PTYHON/teste1.py
@@ -1,55 +1,39 @@
-def q_cédulas():
-    valor = int(input("Digite o quantos você deseja tira do saldo (acima de R$3,99): R$"))
+def caixa_eletronico():
+    valor_saque = int(input("Digite o quanto você deseja tirar do saldo (acima de R$3,99): R$"))
 
-    cédulas = []
-    while valor > 0:
-        if (valor >= 200) and (valor % 2 == 0):      
-            valor -= 200
-            cédulas.append(200)
-        elif (valor >= 200) and (valor % 2 == 1):
-            valor -= 200
-            cédulas.append(200)      
-        elif 100 <= valor < 200:
-            if valor % 2 == 0:       
-                valor -= 100
-                cédulas.append(100)
-            else:
-                valor -= 50
-                cédulas.append(50)    
-        elif 50 <= valor < 100:         
-            valor -= 50
-            cédulas.append(50)
-        elif 20 <= valor < 50:
-            if valor % 2 == 0:       
-                valor -= 20
-                cédulas.append(20)
-            else:
-                valor -= 2
-                cédulas.append(10)
-        elif 10 <= valor < 20:
-            if valor % 2 == 0:
-                    valor -= 2
-                    cédulas.append(10)
-            else:
-                valor -= 5
-                cédulas.append(5)
-        elif 2 <= valor < 10:
-            if valor % 2 == 1:
-                valor -= 5
-                cédulas.append(5)
-            else:
-                valor -= 2
-                cédulas.append(2)
-        else:
-            print("Não é possível fornecer cédula!")
-            break
-    return cédulas
+    cedulas = [200, 100, 50, 20, 10, 5, 2]
+    notas = []
+
+    for nota in cedulas:
+        while valor_saque >= nota:
+            if valor_saque % 2 == 0 and valor_saque == 6 :
+                valor_saque -= 2
+                notas.append(2)
+                break
+            elif valor_saque % 2 == 0 and valor_saque == 8 :
+                valor_saque -= 2
+                notas.append(2)
+                break
+            elif valor_saque % 2 == 1 and 10 < valor_saque < 13:
+                valor_saque -= 5
+                notas.append(5)
+                break
+            elif valor_saque % 2 == 0:
+                valor_saque -= nota
+                notas.append(nota)
+            elif valor_saque % 2 == 1:
+                valor_saque -= nota
+                notas.append(nota)
+    return notas
+
 def main():
-    cédulas = q_cédulas()
-    cédulas_unicas = set(cédulas)
-    print("Você receberá", end="")
-    for elemento in cédulas_unicas:
-        quantidade = cédulas.count(elemento)
-        print(f", {quantidade} cédulas de {elemento} reais", end="")
-    print(".")
-main()
+    notas = caixa_eletronico()
+    
+    if notas:
+        print("Notas retiradas:", notas)
+    else:
+        print("Valor de saque inválido. Certifique-se de que é acima de R$3,99 e um valor permitido.")
+
+if __name__ == "__main__":
+    main()
+
