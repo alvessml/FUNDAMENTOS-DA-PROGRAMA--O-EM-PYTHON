fb6e1d26ac59db7a94a0a596db00e3277091fe89
Samuel Alves <alvess7221@gmail.com>
2023-12-12 18:16:11 -0300

    commit 21.3 - 12/12/2023

--- EXERCÍCIOS EM PTYHON/3.5 .py
+++ EXERCÍCIOS EM PTYHON/3.5 .py
@@ -7,9 +7,28 @@ def usuário():
     return imc
 
 def calculo(imc):
-    
-    return classificação
+
+    if imc < 18.5:
+        classificação = "magreza"
+        grau_de_obesidade = 0
+    elif 18.5 <= imc < 25:
+        classificação = "normal"
+        grau_de_obesidade = 0
+    elif 25 <= imc < 30:
+        classificação = "sobrepeso"
+        grau_de_obesidade = 1    
+    elif 30 <= imc < 40:
+        classificação = "obesidade"
+        grau_de_obesidade = 2
+    else:
+        classificação = "obesidade grave"
+        grau_de_obesidade = 3
+    return classificação, grau_de_obesidade
 
 def main():
     imc = usuário()
-    classificação = calculo(imc)
+    classificação, grau_de_obesidade = calculo(imc)
+
+    print(f"Seu imc é {imc:.2f}, você está {classificação} e com grau {grau_de_obesidade}!")
+
+main()
