2275be21206b0477ce8eb57cf303032be9dc4fcc
Samuel Alves <alvess7221@gmail.com>
2023-12-17 09:52:49 -0300

    commit 23.2 - 17/12/2023

--- EXERCÍCIOS EM PTYHON/4.3 .py
+++ EXERCÍCIOS EM PTYHON/4.3 .py
@@ -1,18 +1,15 @@
 from random import randint
 
+def main(vetor, x = 0, maior=None):
+    if x == len(vetor):
+        return maior
+    else:
+        num_atual = vetor[x]
+        if maior is None or num_atual > maior:
+            maior = num_atual
+            return main(vetor, x + 1, maior)
+
 vetor = [randint(1, 100) for _ in range(10)]
-
+maior_valor = main(vetor)
 print(f"Vetor gerado: {vetor}")
-
-def maior_(n):
-    maior = 0
-    if  vetor[n] > maior:
-        maior = vetor[n]
-    
-    return maior
-
-res = 0
-for i in range(10):
-    res = maior_(i)
-
-print(f"Maior número do meu vetor é {res}.")
+print(f"\nMaior valor gerado foi {maior_valor}.")
